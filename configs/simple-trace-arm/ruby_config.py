99af8ca712466a22c87adeafd6c7ba2377462275
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2023-08-28 12:02:33 +0200

    Starting a new simple architecture with arm and tgen.

---
+++ configs/simple-trace-arm/ruby_config.py
@@ -0,0 +1,169 @@
+import math
+
+from m5.defines import buildEnv
+from m5.util import fatal, panic
+from m5.object import *
+
+from topologies import *
+from network import Network
+
+from . import CHI_config as chi_defs
+
+class MyNetwork(SimpleNetwork):
+    """A simple point-to-point network. This doesn't not use garnet."""
+
+    def __init__(self, ruby_system):
+        super(MyNetwork, self).__init__()
+        self.netifs = []
+        self.ruby_system = ruby_system
+
+    def connectControllers(self, controllers):
+        """Connect all of the controllers to routers and connec the routers
+        together in a point-to-point network.
+        """
+        # Create one router/switch per controller in the system
+        self.routers = [Switch(router_id=i) for i in range(len(controllers))]
+        print("######################################")
+        print("routers", self.routers)
+
+        # Make a link from each controller to the router. The link goes
+        # externally to the network.
+        self.ext_links = [
+            SimpleExtLink(link_id=i, ext_node=c, int_node=self.routers[i])
+            for i, c in enumerate(controllers)
+        ]
+        print("Ext Links", self.ext_links)
+
+        # Make an "internal" link (internal to the network) between every pair
+        # of routers.
+        link_count = 0
+        int_links = []
+        for ri in self.routers:
+            for rj in self.routers:
+                if ri == rj:
+                    continue  # Don't connect a router to itself!
+                link_count += 1
+                int_links.append(
+                    SimpleIntLink(link_id=link_count, src_node=ri, dst_node=rj)
+                )
+        self.int_links = int_links
+        print("Int Links", self.int_links)
+
+
+class L1CacheTrace(RubyCache):
+
+    _version = 0
+
+    @classmethod
+    def versionCount(cls):
+        cls._version += 1  # Use count for this particular type
+        return cls._version - 1
+
+    def __init__(self, system, ruby_system, cpus):
+        """CPUs are needed to grab the clock domain and system is needed for
+        the cache block size.
+        """
+        super(L1CacheTrace, self).__init__()
+
+        self.version = self.versionCount()
+        # This is the cache memory object that stores the cache data and tags
+        self.cacheMemory = RubyCache(
+            size="16kB", assoc=8, start_index_bit=self.getBlockSizeBits(system)
+        )
+        self.clk_domain = system.clk_domain
+        self.send_evictions = self.sendEvicts(cpus)
+        self.ruby_system = ruby_system
+        self.connectQueues(ruby_system)
+
+    def getBlockSizeBits(self, system):
+        bits = int(math.log(system.cache_line_size, 2))
+        if 2**bits != system.cache_line_size.value:
+            panic("Cache line size not a power of 2!")
+        return bits
+
+    def sendEvicts(self, cpus):
+        """True if the CPU model or ISA requires sending evictions from caches
+        to the CPU. Two scenarios warrant forwarding evictions to the CPU:
+        1. The O3 model must keep the LSQ coherent with the caches
+        2. The x86 mwait instruction is built on top of coherence
+        3. The local exclusive monitor in ARM systems
+
+        As this is an X86 simulation we return True.
+        """
+        return True
+
+    def connectQueues(self, ruby_system):
+        """Connect all of the queues for this controller."""
+        self.mandatoryQueue = MessageBuffer()
+
+        # To Network
+        self.reqOut = MessageBuffer(ordered=True)
+        self.reqOut.out_port = ruby_system.network.in_port
+        self.rspOut = MessageBuffer(ordered=True)
+        self.rspOut.out_port = ruby_system.network.in_port
+        self.snpOut = MessageBuffer(ordered=True)
+        self.snpOut.out_port = ruby_system.network.in_port
+        self.datOut = MessageBuffer(ordered=True)
+        self.datOut.out_port = ruby_system.network.in_port
+
+        # From Network
+        self.reqIn = MessageBuffer(ordered=True)
+        self.reqIn.in_port = ruby_system.network.out_port
+        self.rspIn = MessageBuffer(ordered=True)
+        self.rspIn.in_port = ruby_system.network.out_port
+        self.snpIn = MessageBuffer(ordered=True)
+        self.snpIn.in_port = ruby_system.network.out_port
+        self.datIn = MessageBuffer(ordered=True)
+        self.datIn.in_port = ruby_system.network.out_port
+
+
+class DirController(Directory_Controller):
+
+    _version = 0
+
+    @classmethod
+    def versionCount(cls):
+        cls._version += 1  # Use count for this particular type
+        return cls._version - 1
+
+    def __init__(self, ruby_system, ranges, mem_ctrls):
+        """ranges are the memory ranges assigned to this controller."""
+        if len(mem_ctrls) > 1:
+            panic("This cache system can only be connected to one mem ctrl")
+        super(DirController, self).__init__()
+        self.version = self.versionCount()
+        self.addr_ranges = ranges
+        self.ruby_system = ruby_system
+        self.directory = RubyDirectoryMemory()
+        # Connect this directory to the memory side.
+        self.memory = mem_ctrls[0].port
+        self.connectQueues(ruby_system)
+
+    def connectQueues(self, ruby_system):
+        # To Network
+        self.reqOut = MessageBuffer(ordered=True)
+        self.reqOut.out_port = ruby_system.network.in_port
+        self.rspOut = MessageBuffer(ordered=True)
+        self.rspOut.out_port = ruby_system.network.in_port
+        self.snpOut = MessageBuffer(ordered=True)
+        self.snpOut.out_port = ruby_system.network.in_port
+        self.datOut = MessageBuffer(ordered=True)
+        self.datOut.out_port = ruby_system.network.in_port
+
+        # From Network
+        self.reqIn = MessageBuffer(ordered=True)
+        self.reqIn.in_port = ruby_system.network.out_port
+        self.rspIn = MessageBuffer(ordered=True)
+        self.rspIn.in_port = ruby_system.network.out_port
+        self.snpIn = MessageBuffer(ordered=True)
+        self.snpIn.in_port = ruby_system.network.out_port
+        self.datIn = MessageBuffer(ordered=True)
+        self.datIn.in_port = ruby_system.network.out_port
+
+
+        # These are other special message buffers. They are used to send
+        # requests to memory and responses from memory back to the controller.
+        # Any messages sent or received on the memory port (see self.memory
+        # above) will be directed through these message buffers.
+        self.requestToMemory = MessageBuffer()
+        self.responseFromMemory = MessageBuffer()
---
+++ configs/simple-trace-arm/trace_arm.py
@@ -0,0 +1,93 @@
+import math
+
+from m5.object import *
+
+import os
+
+import m5
+from m5.objects import *
+from m5.util import addToPath
+from m5.stats import periodicStatDump
+
+addToPath("../")
+from common import ObjectList
+from common import MemConfig
+
+addToPath("../../")
+import pdb; pdb.set_trace()
+import protolib
+
+from common.FileSystemConfig import config_filesystem
+
+addToPath("../../traces")
+import packet_pb2
+
+from ruby_config import MyNetwork, L1CacheTrace, DirController
+
+np = 1
+system = System(
+    tgens = [TrafficGen(config_file="./m5out/lat_mem_rd.cfg", progess_check="10s") for i in range(1)]
+    cpus = [X86TimingSimpleCPU() for i in range(np)]
+)
+
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = "1GHz"
+system.clk_domain.voltage_domain = VoltageDomain()
+
+# System setup
+system.mem_mode = "timing"
+system.mem_ranges = [AddrRange("512MB")]
+
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.dram = DDR3_1600_8x8()
+system.mem_ctrl.dram.range = system.mem_ranges[0]
+
+# Create and interrupt controller for each cpu and connect it to the membus
+for cpu in system.cpus:
+    cpu.createInterruptController()
+
+# Create the Ruby system
+system.ruby = RubySystem()
+
+system.ruby.network = MyNetwork(system.ruby)
+
+# Network configuration
+# virtual networks: 0=request, 1=snoop, 2=response, 3=data
+system.ruby.number_of_virtual_networks = 4
+
+system.ruby.controllers = \
+    [L1CacheTrace(system, system.caches, cpu) for cpu in system.cpu] + \
+    [DirController(system.ruby, system.mem_ranges, [system.mem_ctrl])] + \
+    [L1CacheTrace(system, system.caches, cpu) for cpu in system.cpu] 
+
+# When np = 1
+#   0      1      2    
+# CPU0 | DIR | TGEN0 
+
+# Create one sequencer per CPU
+system.ruby.sequencers = [
+    RubySequencer(
+        version=i,
+        dcache=system.ruby.controllers[i].cacheMemory,
+        clk_domain=system.ruby.controllers[i].clk_domain,
+    ) 
+    for i in range(np)
+] + [
+   RubySequencer(
+       version=i,
+       dcache=system.ruby.controllers[i+1].cacheMemory,
+       clk_domain=system.ruby.controllers[i+1].clk_domain,
+   ) 
+   for i in range(2, 3)
+]
+
+system.caches.controllers[0].sequencer = system.ruby.seqencers[0] 
+system.caches.controllers[2].sequencer = system.ruby.seqencers[1]
+
+system.ruby.num_of_sequencer = len(system.ruby.sequencers)
+
+system.ruby.network.connectControllers(system.caches.controllers)
+system.ruby.network.setup_buffers()
+
+system.ruby.sys_port_proxy = RubyPortProxy()
+system.system_port = system.ruby.sys_port_proxy.in_ports
