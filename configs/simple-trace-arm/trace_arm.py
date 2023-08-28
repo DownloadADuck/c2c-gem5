ed48cda4f5fb367d604472d8274043252f81c4f2
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2023-08-28 13:48:16 +0200

    Finished scripts. First testings with rudimentary controller setup.

--- configs/simple-trace-arm/trace_arm.py
+++ configs/simple-trace-arm/trace_arm.py
@@ -91,3 +91,41 @@ system.ruby.network.setup_buffers()
 
 system.ruby.sys_port_proxy = RubyPortProxy()
 system.system_port = system.ruby.sys_port_proxy.in_ports
+
+# Connect the cpu's cache, interrupt, and TLB ports to Ruby
+for i, cpu in enumerate(system.cpus):
+    cpu.icache_port = system.caches.sequencers[i].in_ports
+    cpu.dcache_port = system.caches.sequencers[i].in_ports
+    cpu.mmu.itb.walker.port = system.caches.sequencers[i].in_ports
+    cpu.mmu.dtb.walker.port = system.caches.sequencers[i].in_ports
+    cpu.interrupts[0].pio = system.caches.sequencers[i].interrupt_out_port
+    cpu.interrupts[0].int_responder = system.caches.sequencers[i].interrupt_out_port
+    cpu.interrupts[0].int_requestor = system.caches.sequencers[i].in_ports
+
+system.tgen[0].port = system.ruby.sequencers[2].in_ports
+
+thispath = os.path.dirname(os.path.realpath(__file__))
+binary = os.path.join(
+    thispath,
+    "../../",
+    "tests/test-progs/threads/bin/x86/threads",
+)
+
+process = Process()
+process.cmd = [binary]
+for cpu in system.cpus:
+    cpu.workload = process
+    cpu.createThreads()
+
+system.workload = SEWorkload.init_compatible(binary)
+
+config_filesystem(system)
+
+root = Root(full_system=False, system=system)
+
+m5.instantiate()
+print("Begining simulation!")
+exit_event = m5.simulate()
+print(
+    "Exiting @ tick {} because {}".format(m5.curTick(), exit_event.getCause())
+)
