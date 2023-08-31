fd7f32851d983477ba37d6e8506bebff40e7ebef
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2023-08-31 17:16:02 +0200

    Added swp files to gitignore. Tgen_arm.py finished and not tested. Starting ruby_config.py.

--- configs/simple-tgen-arm/tgen_arm.py
+++ configs/simple-tgen-arm/tgen_arm.py
@@ -22,8 +22,62 @@ from common.FileSystemConfig import config_filesystem
 from common.Caches import *
 from common.cpu2000 import *
 
+def get_processes(args):
+    """Interprets provided args and returns a list of processes"""
+
+    multiprocesses = []
+    inputs = []
+    outputs = []
+    errouts = []
+    pargs = []
+
+    workloads = args.cmd.split(";")
+    if args.input != "":
+        inputs = args.input.split(";")
+    if args.output != "":
+        outputs = args.output.split(";")
+    if args.errout != "":
+        errouts = args.errout.split(";")
+    if args.options != "":
+        pargs = args.options.split(";")
+
+    idx = 0
+    for wrkld in workloads:
+        process = Process(pid=100 + idx)
+        process.executable = wrkld
+        process.cwd = os.getcwd()
+        process.gid = os.getgid()
+
+        if args.env:
+            with open(args.env, "r") as f:
+                process.env = [line.rstrip() for line in f]
+
+        if len(pargs) > idx:
+            process.cmd = [wrkld] + pargs[idx].split()
+        else:
+            process.cmd = [wrkld]
+
+        if len(inputs) > idx:
+            process.input = inputs[idx]
+        if len(outputs) > idx:
+            process.output = outputs[idx]
+        if len(errouts) > idx:
+            process.errout = errouts[idx]
+
+        multiprocesses.append(process)
+        idx += 1
+
+    if args.smt:
+        assert args.cpu_type == "DerivO3CPU"
+        return multiprocesses, idx
+    else:
+        return multiprocesses, 1
+
 multiprocesses = []
 numThreads = 1
+
+cmd = "tests/tests-progs/hello/bin/x86/linux/hello"
+multiprocesses, numThreads = get_processes(cmd)
 
 # Number of cpus
 np = 2
@@ -35,7 +89,7 @@ system = System(
             progress_check="10s",
         ) for i in range(np)
     ],
-    cpu=[AtomicSimpleCPU(cpu_id=i) for i in range(np)],
+    cpus=[AtomicSimpleCPU(cpu_id=i) for i in range(np)],
     mem_mode="atomic",
     mem_ranges=[AddrRange('512MB')],
     cache_line_size=64
@@ -47,3 +101,63 @@ if numThreads > 1:
 # Top level voltage domain
 system.voltage_domain = VoltageDomain(voltage='1.0V')
 
+# Source clock for the system
+system.clk_domain = SrcClockDomain(
+    clock='1GHz', voltage_domain=system.voltage_domain
+)
+
+# CPU voltage domain
+system.cpu_voltage_domain = VoltageDomain()
+
+# Separate CPU clock domain
+system.cpu_clk_domain = SrcClockDomain(
+    clock='2GHz', voltage_domain=system.cpu_voltage_domain
+)
+
+for cpu in system.cpus:
+    cpu.clk_domain = system.cpu_clk_domain
+
+for i in range(np):
+    if len(multiprocesses) == 1:
+        system.cpu[i].workload = multiprocesses[0]
+    else:
+        system.cpu[i].workload = multiprocesses[i]
+    system.cpu[i].createTreads()
+
+class Object(object):
+    pass
+
+# Needed options for the create_system method
+options = Object()
+options.caches = True
+options.access_backing_store = False
+options.topology = 'Pt2Pt'
+options.link_latency = 1
+options.router_latency = 1
+options.outdir = "/m5out"
+options.cpu_clock = '2GHz'
+options.l2_size = '2MB'
+options.network = 'simple'
+options.simple_physical_channels = False
+options.repeat_switch = None
+options.take_checkpoints = None
+
+ruby_config.create_system(options, False, system)
+
+system.ruby.clk_domain = SrcClockDomain(
+    clock='2GHz', voltage_domain=system.voltage_domain
+)
+for i in range(np):
+    ruby_port = system.ruby._cpu_ports[i]
+    # Interrupt controller needs its message port conected only with x86
+    system.cpu[i].createInterruptController()
+    ruby_port.connectCpuPorts(system.cpu[i])
+
+system.workload = SEWorkload.init_compatible(mp0_path)
+
+wait_gdb = False
+if wait_gdb:
+    system.workload.wait_for_remote_gdb = True
+    
+root = Root(full_system=False, system=system)
+simulation.run(options, root, system, FutureClass)
