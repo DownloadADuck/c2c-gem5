3b3a818935a45cc54e778312c92419541a01d267
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2023-09-04 15:50:59 +0200

    Starting InterfaceController.cc taking inspo from Directory_Controller.cc

---
+++ src/mem/ruby/fwd_interface/InterfaceController.cc
@@ -0,0 +1,33 @@
+#include <sys/types.hh>
+#include <unistd.h>
+
+#include <cassert>
+#include <sstream>
+#include <string>
+#include <typeinfo>
+
+#include "mem/ruby/common/boolVec.hh"
+
+#include "base/compiler.hh"
+#include "base/cprintf.hh"
+
+#include "debug/RubyGenerated.hh"
+#include "RubySlicc.hh"
+#include "mem/ruby/network/Network.hh"
+#include "mem/ruby/protocol/Interface_Controller.hh"
+#include "mem/ruby/protocol/Interface_Event.hh"
+#include "mem/ruby/protocol/Interface_State.hh"
+#include "mem/ruby/protocol/Types.hh"
+#include "mem/ruby/system/RubySystem.hh"
+
+#include "mem/ruby/slicc_interface/RubySlicc_includes.hh"
+namespace gem5
+{
+    
+namespace ruby
+{
+
+
+
+}
+}
