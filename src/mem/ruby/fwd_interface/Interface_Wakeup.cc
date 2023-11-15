45c207f7306ef9a5206fad63f6583dd5ec9fcc9b
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2023-11-15 11:25:42 +0100

    Finished modifying the wakeups for the interface.

--- src/mem/ruby/fwd_interface/Interface_Wakeup.cc
+++ src/mem/ruby/fwd_interface/Interface_Wakeup.cc
@@ -48,7 +48,7 @@ Interface_Controller::wakeup()
             break;
         }
         // Interface reqIn port
-        m_cur_in_port = 0;
+        m_cur_in_port = 4;
         try
         {
             if ((((*m_reqIn_ptr)).isReady((clockEdge()))))
@@ -93,7 +93,7 @@ Interface_Controller::wakeup()
         }
 
         // Interface snpIn port 
-        m_cur_in_port = 0;
+        m_cur_in_port = 3;
         try
         {
             if ((((*m_snpIn_ptr)).isReady((clockEdge()))))
@@ -138,7 +138,7 @@ Interface_Controller::wakeup()
         }
 
         // Interface rspIn port 
-        m_cur_in_port = 0;
+        m_cur_in_port = 2;
         try
         {
             if ((((*m_rspIn_ptr)).isReady((clockEdge()))))
@@ -183,7 +183,7 @@ Interface_Controller::wakeup()
         }
 
         // Interface datIn port 
-        m_cur_in_port = 0;
+        m_cur_in_port = 1;
         try
         {
             if ((((*m_datIn_ptr)).isReady((clockEdge()))))
@@ -226,6 +226,120 @@ Interface_Controller::wakeup()
         {
             rejected[0]++;
         }
+
+        // BRIDGE side port wakeups
+        // Interface fromBridge
+        m_cur_in_port = 0;
+        try
+        {
+            if ((((*m_datIn_ptr)).isReady((clockEdge()))))
+            {
+                {
+                    // Declare message
+                    [[maybe_unused]] const MemoryMsg *in_msg_ptr;
+                    in_msg_ptr = dynamic_cast<const MemoryMsg *>(((*m_fromBridge_ptr)).peek());
+                    if (in_msg_ptr == NULL)
+                    {
+                        // If the cast fails, this is the wrong inport (wrong message type).
+                        // Throw an exception, and the caller will decide to either try a
+                        // different inport or punt.
+                        throw RejectException();
+                    }
+                    {
+                        if((*in_msg_ptr).m_Type == MemoryRequestType_MEMORY_READ) {
+                            // MemoryRequestType_MEMORY_READ -> Request
+                            {
+                                TransitionResult result = doTransition(Interface_Event_requestToNetwork, ((*in_msg_ptr)).m_addr);
+
+                                if (result == TransitionResult_Valid)
+                                {
+                                    counter++;
+                                    continue; // Check the first port again
+                                }
+                                else if (result == TransitionResult_ResourceStall)
+                                {
+
+                                    scheduleEvent(Cycles(1));
+                                }
+                                else if (result == TransitionResult_ProtocolStall)
+                                {
+
+                                    scheduleEvent(Cycles(1));
+                                }
+                            };
+                        } else if ((*in_msg_ptr).m_Type == MemoryRequestType_MEMORY_WB) {
+                            // MemoryRequestType_MEMORY_WB -> Response
+                            {
+                                TransitionResult result = doTransition(Interface_Event_responseToNetwork, ((*in_msg_ptr)).m_addr);
+
+                                if (result == TransitionResult_Valid)
+                                {
+                                    counter++;
+                                    continue; // Check the first port again
+                                }
+                                else if (result == TransitionResult_ResourceStall)
+                                {
+
+                                    scheduleEvent(Cycles(1));
+                                }
+                                else if (result == TransitionResult_ProtocolStall)
+                                {
+
+                                    scheduleEvent(Cycles(1));
+                                }
+                            };
+                        } else if ((*in_msg_ptr).m_Type == MemoryRequestType_MEMORY_DATA) {
+                            // MemoryRequestType_MEMORY_DATA -> Data
+                            {
+                                TransitionResult result = doTransition(Interface_Event_dataToNetwork, ((*in_msg_ptr)).m_addr);
+
+                                if (result == TransitionResult_Valid)
+                                {
+                                    counter++;
+                                    continue; // Check the first port again
+                                }
+                                else if (result == TransitionResult_ResourceStall)
+                                {
+
+                                    scheduleEvent(Cycles(1));
+                                }
+                                else if (result == TransitionResult_ProtocolStall)
+                                {
+
+                                    scheduleEvent(Cycles(1));
+                                }
+                            };
+                        } else {
+                            // MemoryRequestType_MEMORY_ACK -> Snoop
+                            {
+                                TransitionResult result = doTransition(Interface_Event_snoopToNetwork, ((*in_msg_ptr)).m_addr);
+
+                                if (result == TransitionResult_Valid)
+                                {
+                                    counter++;
+                                    continue; // Check the first port again
+                                }
+                                else if (result == TransitionResult_ResourceStall)
+                                {
+
+                                    scheduleEvent(Cycles(1));
+                                }
+                                else if (result == TransitionResult_ProtocolStall)
+                                {
+
+                                    scheduleEvent(Cycles(1));
+                                }
+                            };
+                        }
+                    };
+                }
+            }
+        }
+        catch (const RejectException &e)
+        {
+            rejected[0]++;
+        }
+
 
         // If we got this far, we have nothing left todo or something went
         // wrong
