72a3c70a04039ab2af6c25e0ba143c3af7935c12
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2023-11-16 10:19:16 +0100

    Added the new ports, corrected the naming.

--- src/mem/ruby/fwd_interface/Interface_Bridge.py
+++ src/mem/ruby/fwd_interface/Interface_Bridge.py
@@ -6,5 +6,8 @@ class InterfaceBridge(SimObject):
     cxx_header = "mem/ruby/fwd_interface/Interface_Bridge.hh"
     cxx_class = "gem5::InterfaceBridge"
 
-    chip0_port = ResponsePort("chip0 side port, receives requests")
-    chip1_port = RequestPort("chip1 side port, sends requests")
+    chip0request = RequestPort("chip0 side port, receives requests")
+    chip0response = ResponsePort("chip0 side port, sends responses")
+
+    chip1request = RequestPort("chip1 side port, receives requests")
+    chip1response = ResponsePort("chip1 side port, sends responses")
