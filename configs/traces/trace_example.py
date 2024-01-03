f75cc6adb8a8279ee29c839186574ff8383ce2cb
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2024-01-03 12:36:35 +0100

    Generating a bi trace again. We can now observe accesses to both memory controllers.

--- configs/traces/trace_example.py
+++ configs/traces/trace_example.py
@@ -149,7 +149,6 @@ def is_pow2(num):
 # assume we start every range at 0
 #max_range = int(mem_range.end)
 max_range = 1024*2
-#max_range = 1024*4
 # start at a size of 4 kByte, and go up till we hit the max, increase
 # the step every time we hit a power of two
 min_range = 1024
@@ -192,8 +191,8 @@ def create_trace(filename, max_addr, burst_size, itt):
     protolib.encodeMessage(proto_out, header)
 
     start_addr = 0
-    #end_addr = 4 * 1024 * 1024 # 4MB
-    end_addr = 2 * 1024
+    end_addr = 4 * 1024 * 1024 # 4MB
+    #end_addr = 2 * 1024
 
     # create a list of every single address to touch
     addrs = list(range(start_addr, end_addr, burst_size))
