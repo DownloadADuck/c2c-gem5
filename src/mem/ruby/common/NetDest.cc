c097391c657fe0c4508d224eb45dec7c59a7b690
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2024-01-23 14:34:12 +0100

    whatev

--- src/mem/ruby/common/NetDest.cc
+++ src/mem/ruby/common/NetDest.cc
@@ -52,6 +52,7 @@ NetDest::add(MachineID newElement)
 
     //if (bitIndex(newElement.num) >= m_bits[vecIndex(newElement)].getSize()){
     std::cout << bitIndex(newElement.num) << " < m_bits[" << vecIndex(newElement) << "]" << " = " << m_bits[vecIndex(newElement)] << std::endl;
+    std::cout << bitIndex(newElement.num) << " < " << m_bits[vecIndex(newElement)].getSize() << std::endl;
     //}
 
     assert(bitIndex(newElement.num) < m_bits[vecIndex(newElement)].getSize());
@@ -266,7 +267,9 @@ NetDest::resize()
     assert(m_bits.size() == MachineType_NUM);
 
     for (int i = 0; i < m_bits.size(); i++) {
+        std::cout << "m_bits[" << i << "] -->" << " number of " << (MachineType)i << " is " << MachineType_base_count((MachineType)i) << std::endl;
         m_bits[i].setSize(MachineType_base_count((MachineType)i));
+        //std::cout << "m_bits[" << i << "] size -> " << m_bits[i].getSize() << std::endl;
     }
 }
 
