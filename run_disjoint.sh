1027b10c07e0baf8924532ba74b1eaf9450dda5f
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2024-02-20 07:56:11 +0100

    removing old command file

--- run_disjoint.sh
+++
@@ -1,8 +0,0 @@
-#!usr/bin/env bash
-
-build/VEGA_X86/gem5.opt configs/example/c2ctest/hip_samples.py \
-  --disk-image /home/lbertranalvarez/Work/fs/gpu/rocm42 \
-  --kernel /home/lbertranalvarez/Work/fs/gpu/vmlinux-5.4.0-105-generic \
-  --gpu-mmio-trace /home/lbertranalvarez/Work/fs/gpu/vega_mmio.log \
-  -a PrefixSum \
-  --num-compute-units 1
