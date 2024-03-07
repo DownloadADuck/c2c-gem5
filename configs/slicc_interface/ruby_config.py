09a2d8b86c9ee22324677b4f9df1d7b475b4056d
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2024-03-07 15:29:06 +0100

    Needed to add the new names in the SLICC code.

--- configs/slicc_interface/ruby_config.py
+++ configs/slicc_interface/ruby_config.py
@@ -231,7 +231,7 @@ def create_system(
     # C2C forwarding interface setup
     for interface in system.ruby.interface1:
         for snf3 in system.ruby.snf3:
-            snf3.cntrl.memory_out_port = interface.cntrl.memory_in_port
+            snf3.cntrl.memory_out_port = interface.cntrl.c2c_in_port
 
     # In SE register the ropology elements with fake filesystem
     if not full_system:
