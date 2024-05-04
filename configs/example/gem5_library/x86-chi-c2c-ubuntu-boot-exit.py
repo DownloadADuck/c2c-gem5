f1bd1e469328d973077dd1d700a4316f39647cc5
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2024-05-04 16:36:08 +0200

    Finished main file. Added new components to SConscript

---
+++ configs/example/gem5_library/x86-chi-c2c-ubuntu-boot-exit.py
@@ -0,0 +1,79 @@
+# C2c architecture booting and exiting ubuntu with KVM enabled CPUs
+# Uses X86 and CHI
+from gem5.utils.requires import requires
+from gem5.components.boards.x86_board import X86Board
+from gem5.components.memory.single_channel import SingleChannelDDR3_1600
+from gem5.components.processors.simple_switchable_processor import (
+    SimpleSwitchableProcessor,
+)
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.isas import ISA
+from gem5.coherence_protocol import CoherenceProtocol
+from gem5.simulate.simulator import Simulator
+from gem5.simulate.exit_event import ExitEvent
+from gem5.resources.workload import Workload
+
+# This runs a check to ensure the gem5 binary is compiled to X86 and to the
+# CHI coherence protocol.
+requires(
+    isa_required=ISA.X86,
+    coherence_protocol_required=CoherenceProtocol.CHI,
+    kvm_required=True,
+)
+
+from gem5.components.cacheierarchies.chi.c2c_cache_hierarchy import (
+    C2cCacheHierarchy,
+)
+
+# Here we setup a MESI Two Level Cache Hierarchy.
+cache_hierarchy = C2cCacheHierarchy(
+    l1d_size="16kB",
+    l1d_assoc=8,
+    l1i_size="16kB",
+    l1i_assoc=8,
+    l2_size="256kB",
+    l2_assoc=16,
+    num_l2_banks=1,
+)
+
+# System memory
+memory = SingleChannelDDR3_1600(size="3GB")
+
+# Switchable KVM -> timing
+processor = SimpleSwitchableProcessor(
+    starting_core_type=CPUTypes.KVM,
+    switch_core_type=CPUTypes.TIMING,
+    isa=ISA.X86,
+    num_cores=2,
+)
+
+# Board setup
+board = X86Board(
+    clk_freq="3GHz",
+    processor=processor,
+    memory=memory,
+    cache_hierarchy=cache_hierarchy,
+)
+
+# Full System workload setup
+# The X86Board takes a kernel, a disk image and an optional command to run
+command = (
+    "m5 exit;"
+    + "echo 'This is running on Timing CPU cores.';"
+    + "sleep 1;"
+    + "m5 exit;"
+)
+
+workload = Workload("x86-ubuntu-18.04-boot")
+workload.set_parameter("readfile_contents", command)
+board.set_workload(workload)
+
+simulator = Simulator(
+    board=board,
+    on_exit_event={
+        # Overriding the default behavior for the first m5 exit event. instead
+        # of exiting the simulator we want to switch processor. 
+        ExitEvent.EXIT: (func() for func in [processor.switch])
+    }
+)
+simulator.run()
