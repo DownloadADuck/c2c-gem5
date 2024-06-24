4b93bb076ebd39db3dc4f6db7f747d7b4805d92b
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2024-06-24 11:37:51 +0200

    Hack in RubyPort to select the second network.

--- src/mem/ruby/system/RubyPort.cc
+++ src/mem/ruby/system/RubyPort.cc
@@ -365,6 +365,11 @@ RubyPort::MemResponsePort::recvAtomic(PacketPtr pkt)
     // Find the controller for the target address
     MachineID id = ruby_port->m_controller->mapAddressToMachine(
                     pkt->getAddr(), (MachineType)mem_interface_type);
+    if (ruby_port->m_controller.m_net_ptr.m_version != 0) {
+        if (id->getNum() == 2) {
+            id.num = 1;
+        }
+    } 
     
     AbstractController *mem_interface =
         rs->m_abstract_controls[mem_interface_type][id.getNum()];
