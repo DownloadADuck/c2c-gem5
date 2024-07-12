5c43a08d4b187d042ff945c74277cc1a4a99998e
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2024-07-12 14:34:12 +0200

    Using noncaching_simple cpu models.

--- configs/example/gem5_library/x86_c2c_kvm/no-checkpoint-x86-ubuntu-boot-exit.py
+++ configs/example/gem5_library/x86_c2c_kvm/no-checkpoint-x86-ubuntu-boot-exit.py
@@ -47,7 +47,7 @@ memory = DualChannelDDR3_1600_C2C(size="3GB", range_size="1610612736")
 # Switchable KVM -> timing
 processor = SimpleSwitchableProcessor(
     starting_core_type=CPUTypes.KVM,
-    switch_core_type=CPUTypes.KVM,
+    switch_core_type=CPUTypes.NONCACHING_SIMPLE,
     isa=ISA.X86,
     num_cores=2,
 )
