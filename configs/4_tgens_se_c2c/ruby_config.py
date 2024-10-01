327c0528e40b166bbac97289a18d5763dd00b17c
DownloadADuck <luis.bertran-alvarez@outlook.fr>
2024-10-01 17:07:52 +0200

    New traces generator for the hnf study, new scripts with 4 requestors

---
+++ configs/4_tgens_se_c2c/ruby_config.py
@@ -0,0 +1,321 @@
+import math
+import m5
+
+from m5.objects import *
+from m5.defines import buildEnv
+from m5.util import addToPath, fatal
+from gem5.isas import ISA
+from gem5.runtime import get_runtime_isa
+
+from two_tgens_se_c2c import chip_config
+from CHI_config import MemCtrlMessageBuffer
+
+addToPath("../")
+from common import ObjectList
+from common import MemConfig
+from common import FileSystemConfig
+
+from topologies import *
+from network import Network
+
+def setup_memory_controllers(
+    system,
+    ruby, 
+    dir_cntrls0, 
+    dir_cntrls1, 
+    mem_ranges0,
+    mem_ranges1,
+    options0,
+    options1
+):
+    ruby.block_size_bytes = options0.cacheline_size
+    ruby.memory_size_bits = 48
+
+    index = 0
+    mem_ctrls0 = []
+    mem_ctrls1 = []
+    crossbars = []
+
+    intlv_size = options0.cacheline_size
+
+    for i, dir_cntrl in enumerate(dir_cntrls0):
+        crossbar = None
+
+        dir_ranges = []
+        #for mem_range in mem_ranges0:
+        if i != 0:
+            mem_type = ObjectList.mem_list.get(options0.mem_type)
+            range = m5.objects.AddrRange(
+                mem_ranges1.start,
+                size=mem_ranges1.size(),
+            )
+
+            dir_ranges.append(range)
+        else:
+            # classic SN
+            mem_type = ObjectList.mem_list.get(options0.mem_type)
+            dram_intf = MemConfig.create_mem_intf(
+                mem_type,
+                mem_ranges0,
+                i,
+                int(math.log(options0.num_dirs, 2)),
+                intlv_size,
+                options0.xor_low_bit,
+            )
+
+            if issubclass(mem_type, DRAMInterface):
+                mem_ctrl = m5.objects.MemCtrl(dram=dram_intf)
+            else: 
+                mem_ctrl = dram_intf
+
+            mem_ctrls0.append(mem_ctrl)
+            dir_ranges.append(dram_intf.range)
+
+        if crossbar != None:
+            mem_ctrl.port = crossbar.mem_side_ports
+        else:
+            if i == 0:
+                # classic SN
+                mem_ctrl.port = dir_cntrl.memory_out_port
+
+                # Enable low-power DRAM states if option is enabled
+                if issubclass(mem_type, DRAMInterface):
+                    mem_ctrl.dram.enable_dram_powerdown = (
+                        options0.enable_dram_powerdown
+                    )
+        index += 1
+        dir_cntrl.addr_ranges = dir_ranges
+
+    index = 0
+
+    for i, dir_cntrl in enumerate(dir_cntrls1):
+        crossbar = None
+        
+        dir_ranges = []
+        #for mem_range in mem_ranges1:
+        if i != 0:
+            # C2C Interface
+            mem_type = ObjectList.mem_list.get(options1.mem_type)
+            range = m5.objects.AddrRange(
+                mem_ranges0.start,
+                size=mem_ranges0.size(),
+            )
+
+            dir_ranges.append(range)
+        else: 
+            # classic SN
+            mem_type = ObjectList.mem_list.get(options1.mem_type)
+            dram_intf = MemConfig.create_mem_intf(
+                mem_type,
+                mem_ranges1,
+                i,
+                int(math.log(options1.num_dirs, 2)),
+                intlv_size,
+                options1.xor_low_bit,
+            )
+
+            if issubclass(mem_type, DRAMInterface):
+                mem_ctrl = m5.objects.MemCtrl(dram=dram_intf)
+            else: 
+                mem_ctrl = dram_intf
+                
+            mem_ctrls1.append(mem_ctrl)
+            dir_ranges.append(dram_intf.range)
+
+        if crossbar != None:
+            mem_ctrl.port = crossbar.mem_side_ports
+        else:
+            if i == 0:
+                mem_ctrl.port = dir_cntrl.memory_out_port
+
+            # Enable low-power DRAM states if option is enabled
+            if issubclass(mem_type, DRAMInterface):
+                mem_ctrl.dram.enable_dram_powerdown = (
+                    options1.enable_dram_powerdown
+                )
+        index += 1
+        dir_cntrl.addr_ranges = dir_ranges
+
+    system.mem_ctrls0 = mem_ctrls0
+    system.mem_ctrls1 = mem_ctrls1
+    
+    if len(crossbars) > 0:
+        ruby.crossbars = crossbars
+
+def create_topology(controllers, options):
+    exec("import topologies.%s as Topo" % options.topology)
+    topology = eval("Topo.%s(controllers)" % options.topology)
+    return topology
+
+def create_system(
+    options,
+    options1,
+    full_system,
+    system,
+    piobus=None,
+    dma_ports=[],
+    bootmem=None,
+    cpus=None,
+):
+    system.ruby = RubySystem()
+    ruby = system.ruby
+    
+    # Generate pseudo filesystem
+    FileSystemConfig.config_filesystem(system, options)
+
+    # Create the network0 object
+    # Chip 0
+    (
+        network0,
+        IntLinkClass,
+        ExtLinkClass,
+        RouterClass,
+        InterfaceClass,
+    ) = Network.create_network(options, ruby)
+    ruby.network0 = network0
+
+    (
+        network1,
+        IntLinkClass,
+        ExtLinkClass,
+        RouterClass,
+        InterfaceClass,
+    ) = Network.create_network(options1, ruby)
+    ruby.network1 = network1
+
+    if cpus is None:
+        cpus0 = system.cpus0
+        cpus1 = system.cpus1
+    
+    # Chip 0
+    (cpu_sequencers0, dir_cntrls0, topology0) = \
+        chip_config.create_chip0(
+            options, 
+            full_system, 
+            system, 
+            dma_ports, 
+            bootmem, 
+            ruby, 
+            cpus0,
+            network0
+        )
+    
+    # Chip 1
+    (cpu_sequencers1, dir_cntrls1, topology1) = \
+        chip_config.create_chip1(
+            options1,
+            full_system,
+            system,
+            dma_ports,
+            bootmem,
+            ruby,
+            cpus1,
+            network1
+        )
+
+    # Create the network topology
+    topology0.makeTopology(
+        options, 
+        network0,
+        IntLinkClass,
+        ExtLinkClass,
+        RouterClass
+    )
+
+    topology1.makeTopology(
+        options1, 
+        network1, 
+        IntLinkClass, 
+        ExtLinkClass, 
+        RouterClass
+    )
+
+    # C2C forwarding interface setup
+    for interface in system.ruby.interface1:
+        for interface0 in system.ruby.interface0:
+            interface0.cntrl.c2c_out_port = interface.cntrl.c2c_in_port
+            interface0.cntrl.c2c_in_port = interface.cntrl.c2c_out_port
+
+    # In SE register the ropology elements with fake filesystem
+    if not full_system:
+        topology0.registerTopology(options)
+        topology1.registerTopology(options1)
+
+    # Initialize network based topology
+    Network.init_network(options, network0, InterfaceClass)
+    Network.init_network(options1, network1, InterfaceClass)
+
+    # Create a port proxy for connecting the system port.
+    sys_port_proxy = RubyPortProxy(ruby_system=ruby)
+    if piobus is not None:
+        sys_port_proxy.pio_request_port = piobus.cpu_side_ports
+    
+    # Give the system port proxy a SimObject parent without creating a
+    # full-fledged controller
+    system.sys_port_proxy = sys_port_proxy
+    
+    # Connect the system port for loading of binaries etc
+    system.system_port = system.sys_port_proxy.in_ports
+
+    setup_memory_controllers(
+        system,
+        ruby,
+        dir_cntrls0,
+        dir_cntrls1,
+        system.mem_ranges[0],
+        system.mem_ranges[1],
+        options,
+        options1
+    )
+
+    # Connect the cpu sequencers and the piobus
+    if piobus != None:
+        for cpu_seq in cpu_sequencers0:
+            cpu_seq.connectIOPorts(piobus)
+
+    # TrafficGen setup
+    for i in range(len(cpus0)):
+        system.tgens0[i].port = cpu_sequencers0[i].in_ports
+
+    for i in range(len(cpus1)):
+        system.tgens1[i].port = cpu_sequencers1[i].in_ports
+    
+    ruby.number_of_virtual_networks = ruby.network0.number_of_virtual_networks
+    ruby._cpu_ports = cpu_sequencers0
+    ruby.num_of_sequencers = len(cpu_sequencers0)
+
+
+def create_directories(options, bootmem, ruby_system, system):
+    dir_cntrl_nodes = []
+    for i in range(options.num_dirs):
+        dir_cntrl = Directory_Controller()
+        dir_cntrl.version = i
+        dir_cntrl.directory = RubyDirectoryMemory()
+        dir_cntrl.ruby_system = ruby_system
+        
+        exec("ruby_system.dir_cntrl%d = dir_cntrl" % i)
+        dir_cntrl_nodes.append(dir_cntrl)
+    
+    if bootmem is not None:
+        rom_dir_cntrl = Directory_Controller()
+        rom_dir_cntrl.directoy = RubyDirectoryMemory()
+        rom_dir_cntrl.ruby_system = ruby_system
+        rom_dir_cntrl.version = i + 1
+        rom_dir_cntrl.memory = bootmem.port
+        rom_dir_cntrl.addr_ranges = bootmem.range
+        return (dir_cntrl_nodes, rom_dir_cntrl)
+    
+    return (dir_cntrl_nodes, None) 
+
+
+def send_evicts(options):
+    # Forwarding evictions to the CPU happens when:
+    # 1. The O3 model must keep the LSQ coherent with the caches
+    # 2. The x86 mwait intruction is built on top of coherence invalidations
+    # 3. The local exclusive monitor in ARM systems
+    if options.cpu_type == "DerivO3CPU" or get_runtime_isa() in (
+        ISA.X86,
+        ISA.ARM,
+    ):
+        return True
+    return False
---
+++ configs/traces/hnf_study.py
@@ -0,0 +1,185 @@
+import gzip
+import argparse
+import os
+import sys
+
+import m5
+from m5.objects import *
+from m5.util import addToPath
+from m5.stats import periodicStatDump
+
+addToPath("../")
+from common import ObjectList
+from common import MemConfig
+
+addToPath("../../util")
+import protolib
+
+from ruby import Ruby
+
+try:
+    import packet_pb2
+except:
+    print("Did not find packet proto definitions, attempting to generate")
+    from subprocess import call
+
+    error = call(
+        [
+            "protoc",
+            "--python_out=configs/traces",
+            "--proto_path=src/proto",
+            "src/proto/packet.proto",
+        ]
+    )
+    if not error:
+        print("Generated packet proto definitions")
+        try:
+            import google.protobuf
+        except:
+            print("Please install the Python protobuf module")
+            exit(-1)
+        import packet_pb2
+    else:
+        print("Failed to import packet proto definitions")
+        exit(-1)
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--mem-type",
+    default="DDR3_1600_8x8",
+    choices=ObjectList.mem_list.get_names(),
+    help="type of memory to use",
+)
+parser.add_argument(
+    "--mem-size",
+    action="store",
+    type=str,
+    default="2GB",
+    help="Specify the memory size per chip (total 3GB)",
+)
+parser.add_argument(
+    "--iterations",
+    action="store",
+    type=int,
+    default=10,
+    help="Number of iterations (accesses) per memory region",
+)
+args = parser.parse_args()
+
+def create_trace(filenames, packets, burst_size):
+    proto_outs = {}
+    for filename in filenames.values():
+        try:
+            proto_outs[filename] = gzip.open(filename, "wb")
+        except IOError:
+            print(f"Failed to open {filename} for writing")
+            exit(-1)
+
+        proto_outs[filename].write(b'gem5')
+
+        header = packet_pb2.PacketHeader()
+        header.obj_id = "lat_mem_rd trace"
+        header.tick_freq = 1000000000000
+        protolib.encodeMessage(proto_outs[filename], header)
+
+    for packet_info in packets:
+        packet = packet_pb2.Packet()
+        packet.tick = packet_info['tick']
+        packet.addr = packet_info['addr']
+        packet.size = packet_info['size']
+        packet.cmd = packet_info['cmd']
+
+        core_type = packet_info['core_type']
+        filename = filenames[core_type]
+        protolib.encodeMessage(proto_outs[filename], packet)
+
+    for proto_out in proto_outs.values():
+        proto_out.close()
+
+def generate_packets():
+    packets = []
+    base_tick = 5000
+    addr = 2 * 1024**3  # Start at 2GB
+
+    # Generate requests for three caches, with 10000 ticks delay between each
+    cache_names = ['cache1', 'cache2', 'cache3']
+    for i, cache_name in enumerate(cache_names):
+        packet_info = {
+            'tick': base_tick + i * 10000,  # Delay of 10000 ticks between requests
+            'addr': addr,
+            'size': 64,
+            'cmd': 1,  # ReadReq
+            'core_type': cache_name
+        }
+        packets.append(packet_info)
+
+    return packets
+
+packets = generate_packets()
+
+# Create trace files for three caches
+trace_files = {
+    'cache1': os.path.join(m5.options.outdir, "lat_mem_rd_cache1.trc.gz"),
+    'cache2': os.path.join(m5.options.outdir, "lat_mem_rd_cache2.trc.gz"),
+    'cache3': os.path.join(m5.options.outdir, "lat_mem_rd_cache3.trc.gz"),
+}
+
+burst_size = 64
+create_trace(trace_files, packets, burst_size)
+
+print("Generated trace files:", trace_files)
+
+def create_cfg_file(cfg_filename, trace_filename, period):
+    with open(cfg_filename, "w") as cfg_file:
+        cfg_file.write(f"STATE 0 {period} TRACE {trace_filename} 0\n")
+        cfg_file.write("INIT 0\n")
+        cfg_file.write("TRANSITION 0 0 1\n")
+
+cfg_files = {
+    'cache1': os.path.join(m5.options.outdir, "lat_mem_rd_cache1.cfg"),
+    'cache2': os.path.join(m5.options.outdir, "lat_mem_rd_cache2.cfg"),
+    'cache3': os.path.join(m5.options.outdir, "lat_mem_rd_cache3.cfg"),
+}
+
+itt = 150 * 1000  # 150 ns in ticks
+period = int(itt * (16384 / burst_size))
+
+for cache_name, trace_file in trace_files.items():
+    create_cfg_file(cfg_files[cache_name], trace_file, period)
+
+print(f"Generated cfg files: {cfg_files}")
+
+# Rest of the simulation setup (unchanged)
+system = System(membus=SystemXBar(width=32))
+system.clk_domain = SrcClockDomain(
+    clock="2.0GHz", voltage_domain=VoltageDomain(voltage="1V")
+)
+
+mem_range = AddrRange(args.mem_size)
+system.mem_ranges = [mem_range]
+system.mmap_using_noreserve = True
+
+args.mem_channels = 1
+args.mem_ranks = 1
+args.external_memory_system = 0
+args.tlm_memory = 0
+args.elastic_trace_en = 0
+
+MemConfig.config_mem(args, system)
+
+# Assign traffic generators to each cache's config file
+system.tgen_cache1 = TrafficGen(config_file=cfg_files['cache1'], progress_check="10s")
+system.tgen_cache2 = TrafficGen(config_file=cfg_files['cache2'], progress_check="10s")
+system.tgen_cache3 = TrafficGen(config_file=cfg_files['cache3'], progress_check="10s")
+
+system.system_port = system.membus.cpu_side_ports
+
+periodicStatDump(period)
+
+root = Root(full_system=False, system=system)
+root.system.mem_mode = "timing"
+
+m5.instantiate()
+m5.simulate(2 * period)
+
+print("lat_mem_rd simulation complete")
