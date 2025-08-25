616c0bf9e8c891c0e76511982bf9ec26c9d90f17
luis.bertran-alvarez@lirmm.fr <lbertranalvarez@gem5.lirmm.fr>
2025-08-25 14:25:20 +0200

    Fixing the transmition of originalRequestor

--- src/mem/ruby/slicc_interface/AbstractController.cc
+++ src/mem/ruby/slicc_interface/AbstractController.cc
@@ -576,6 +576,7 @@ AbstractController::c2cOutRecvTimingResp(PacketPtr pkt)
 
         (*msg).m_Stale = (*(pkt->c2c_msg)).m_Stale;
         (*msg).m_C2c_sharers = (*(pkt->c2c_msg)).m_C2c_sharers;
+        (*msg).m_OriginalRequestor = (*(pkt->c2c_msg)).m_OriginalRequestor;
     } else {
         panic("Incorrect packet type received in the c2c_out_port!");
     }
